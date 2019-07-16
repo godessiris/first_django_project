6091d3fda90a661766eafac373866103590420c1
godessiris <godessiris@gmail.com>
2019-07-16 14:51:31 -0500

    first commit

---
+++ apps/first_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    print("///////")
+    print("Create route hit ...redirecting to /...")
+    return redirect("/")
+
+def show(request,number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    print("*************************************")
+    print(f"route to delete blog {number}")
+    return redirect("/")
